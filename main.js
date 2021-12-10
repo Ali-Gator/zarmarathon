0b3a7f3c5bc180b05c859af458e7dbd1354c85c4
Oleg Gordienov <ogordienov@yandex.ru>
2021-12-10 14:39:05 +0200

    add random fight

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
 
 const player1 = {
   player: 1,
@@ -56,3 +57,39 @@ function createPlayer(playerObj) {
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function changeHP(playerObj) {
+  const $playerLife = document.querySelector(
+    '.player' + playerObj.player + ' .life'
+  );
+  playerObj.hp -= getRandomIntInclusive(1, 20);
+
+  if (playerObj.hp < 0) {
+    playerObj.hp = 0;
+  }
+
+  $playerLife.style.width = playerObj.hp + '%';
+}
+
+function playerWin(name) {
+  const $winTitle = createElement('div', 'winTitle');
+  $winTitle.innerText = name + ' wins';
+  return $winTitle;
+}
+
+$randomButton.addEventListener('click', function () {
+  changeHP(player1);
+  changeHP(player2);
+
+  if (player1.hp === 0) {
+    $arenas.appendChild(playerWin(player2.name));
+    $randomButton.disabled = true;
+  } else if (player2.hp === 0) {
+    $arenas.appendChild(playerWin(player1.name));
+    $randomButton.disabled = true;
+  }
+});
