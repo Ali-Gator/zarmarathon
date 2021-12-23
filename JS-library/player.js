e77ca78832ddfa654aa578dabf2061b0d9297c89
Oleg Gordienov <ogordienov@yandex.ru>
2021-12-23 19:22:04 +0200

    refactoring: start classes

--- JS-library/player.js
+++ JS-library/player.js
@@ -1,41 +1,45 @@
 import createElement from './createElement.js';
 
-function changeHP(value) {
-  this.hp -= value;
-
-  if (this.hp < 0) {
-    this.hp = 0;
+class Player {
+  constructor(props) {
+    this.player = props.player;
+    this.name = props.name;
+    this.hp = props.hp;
+    this.img = props.img;
   }
+
+  changeHP = (value) => {
+    this.hp -= value;
+
+    if (this.hp < 0) {
+      this.hp = 0;
+    }
+  };
+
+  elHP = () => {
+    console.log (this.player);
+    return document.querySelector(`.player${this.player} .life`);
+  };
+
+  renderHP = () => {
+    const a = this.elHP();
+    return (a.style.width = `${this.hp}%`);
+  };
 }
 
-function elHP() {
-  return document.querySelector(`.player${this.player} .life`);
-}
-
-function renderHP() {
-  const a = this.elHP();
-  return (a.style.width = `${this.hp}%`);
-}
-
-export const player1 = {
+export const player1 = new Player({
   player: 1,
   name: 'Subzero',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
-  changeHP,
-  elHP,
-  renderHP,
-};
+});
 
-export const player2 = {
+export const player2 = new Player({
   player: 2,
   name: 'Scorpion',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-  changeHP,
-  elHP,
-  renderHP,
-};
+});
 
 export const createPlayer = ({ player, hp, name, img }) => {
   const $player = createElement('div', `player${player}`);
--- main.js
+++ main.js
@@ -1,36 +1,5 @@
-import { player1, player2, createPlayer } from './JS-library/player.js';
-import generateLogs from './JS-library/logs.js';
-import showResult from './JS-library/result.js';
-import { enemyAttack, playerAttack } from './JS-library/attack.js';
-import { $arenas, $formFight} from './JS-library/DOMelements.js';
+import Game from './JS-library/game.js';
 
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
+const game = new Game;
 
-generateLogs('start', player1, player2);
-
-$formFight.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-const { value: enemyValue, hit: enemyHit, defence: enemyDefence } = enemyAttack();
-const { value: playerValue, hit: playerHit, defence: playerDefence } = playerAttack();
-
-if (enemyHit !== playerDefence) {
-    const damage = enemyValue;
-    player1.changeHP(damage);
-    player1.renderHP();
-    generateLogs('hit', player1, player2, damage);
-  } else {
-    generateLogs('defence', player1, player2);
-  }
-
-  if (playerHit !== enemyDefence) {
-    const damage = playerValue;
-    player2.changeHP(damage);
-    player2.renderHP();
-    generateLogs('hit', player2, player1, damage);
-  } else {
-    generateLogs('defence', player2, player1);
-  }
-  showResult();
-});
+game.start();
